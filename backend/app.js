c7480054007ed17c949b52e762206d803827f997
Johnny <jhnyc@connect.hku.hk>
2022-05-13 00:53:13 +0800

    dynamic route for retrieving different doc

--- backend/app.js
+++ backend/app.js
@@ -17,10 +17,8 @@ AWS.config.update({ region: "ap-east-1" }); // set region
 const s3 = new AWS.S3();
 
 // bucket and file name
-BUCKET_NAME = "lihkg-test";
-FILENAME = "test.json";
-
-// read json object from s3
+BUCKET_NAME = "lihkg-dashboard";
+OUTPUT_DIR = "Output";
 
 // list all buckets
 s3.listBuckets(function(err, data) {
@@ -35,15 +33,26 @@ app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
-app.get("/awstest", (req, res) => {
+// browsers will by default try to request favicon.ico everytime, hence catch the request as invalid request
+app.get("/favicon.ico", (req, res) => {
+    res.send("Invalid request.", 404);
+});
+
+// dynamic route for retrieving different json documents from s3
+app.get("/:id", (req, res) => {
+    var file = `${OUTPUT_DIR}/${req.params.id}.json`;
     const objectParams = {
         Bucket: BUCKET_NAME,
-        Key: "posts_by_weekday_hour.json",
+        Key: file,
     };
     s3.getObject(objectParams, function(err, data) {
-        if (err) res.send(err, err.stack); // an error occurred
+        if (err) res.send("Invalid request.", 404); // an error occurred
         else res.send(JSON.parse(data.Body.toString("utf-8"))); // successful response
     });
+});
+
+app.get("*", function(req, res) {
+    res.send("Invalid request.", 404);
 });
 
 app.listen(port, () => {
