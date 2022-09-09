2c86b1dee0738e30a333914304d98b8a88ebb443
Johnny <jhnyc@connect.hku.hk>
2022-09-08 22:20:51 -0700

    tweak

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,16 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+require("dotenv").config();
 app.use(cors());
-
-const port = 3100;
-
-// read and set aws credentials
-const dotenv = require("dotenv");
-dotenv.config();
 
 // load aws sdk
 const AWS = require("aws-sdk");
-AWS.config.update({ region: "ap-east-1" }); // set region
+AWS.config.update({
+    accessKeyId: process.env.aws_access_key_id,
+    secretAccessKey: process.env.aws_secret_access_key,
+    region: "ap-east-1",
+});
 
 // create s3 object
 const s3 = new AWS.S3();
@@ -39,22 +38,26 @@ app.get("/favicon.ico", (req, res) => {
 });
 
 // dynamic route for retrieving different json documents from s3
+// app.get("/:id", (req, res) => {
+//     var file = `${OUTPUT_DIR}/${req.params.id}.json`;
+//     const objectParams = {
+//         Bucket: BUCKET_NAME,
+//         Key: file,
+//     };
+//     s3.getObject(objectParams, function(err, data) {
+//         if (err) res.send("Invalid request.", 404); // an error occurred
+//         else res.send(JSON.parse(data.Body.toString("utf-8"))); // successful response
+//     });
+// });
 app.get("/:id", (req, res) => {
-    var file = `${OUTPUT_DIR}/${req.params.id}.json`;
-    const objectParams = {
-        Bucket: BUCKET_NAME,
-        Key: file,
-    };
-    s3.getObject(objectParams, function(err, data) {
-        if (err) res.send("Invalid request.", 404); // an error occurred
-        else res.send(JSON.parse(data.Body.toString("utf-8"))); // successful response
-    });
+    var file = require(`./asset/${req.params.id}.json`);
+    res.send(file);
 });
 
 app.get("*", function(req, res) {
     res.send("Invalid request.", 404);
 });
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+app.listen(process.env.http_port, () => {
+  console.log(`Example app listening on port ${process.env.http_port}`);
 });
