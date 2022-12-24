87a1c4123d7bd8a6f3caa4f387bf0a432570ca7a
Johnny <jhnyc@connect.hku.hk>
2022-12-23 18:31:37 -0800

    tweak sidebar

--- frontend/src/components/PostMetrics/Sidebar.jsx
+++ frontend/src/components/PostMetrics/Sidebar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { FaUsers } from "react-icons/fa";
-import { IoChatbubbleEllipsesSharp } from "react-icons/io5";
+import { IoChatbubbleEllipsesSharp, IoLogoGithub } from "react-icons/io5";
 import { RiFolderDownloadFill } from "react-icons/ri";
 import Logo from "../../assets/LIHKG_logo.png";
 import { Link } from "react-router-dom";
@@ -31,60 +31,67 @@ function Sidebar(props) {
 
   return (
     <div className="sidebar">
-      <div
-        className="sidebar-toggle-container"
-        style={{ justifyContent: isOpen ? "flex-end" : "center" }}
-      >
-        <button id="toggle" onClick={toggleOpenOrClose}>
-          <AiOutlineMenu />
-        </button>{" "}
-      </div>
-      <div className="upper__container">
-        <div className="brand">
-          <a href="http://lihkg.com" target="_blank">
-            <img src={Logo} alt="" />
+      <div className="sidebar_container">
+        <div className="upper__container">
+          <div
+            className="sidebar-toggle-container"
+            style={{ justifyContent: isOpen ? "flex-end" : "center" }}
+          >
+            <button id="toggle" onClick={toggleOpenOrClose}>
+              <AiOutlineMenu />
+            </button>{" "}
+          </div>
+          <div className="brand">
+            <a href="http://lihkg.com" target="_blank">
+              <img src={Logo} alt="" />
+            </a>{" "}
+          </div>{" "}
+          <div className="links">
+            <ul>
+              <li
+                id={location.pathname == "/" ? "active" : ""}
+                style={{ justifyContent: isOpen ? "" : "center" }}
+              >
+                {/* <IoChatbubbleEllipsesSharp /> */}{" "}
+                <Link to="/">
+                  <IoChatbubbleEllipsesSharp />
+                  <span style={{ display: isOpen ? "" : "none" }}>
+                    Post Insights{" "}
+                  </span>{" "}
+                </Link>{" "}
+              </li>{" "}
+              <li
+                id={location.pathname == "/usermetrics" ? "active" : ""}
+                style={{ justifyContent: isOpen ? "" : "center" }}
+              >
+                <Link to="/usermetrics">
+                  <FaUsers />
+                  <span style={{ display: isOpen ? "" : "none" }}>
+                    User Insights{" "}
+                  </span>{" "}
+                </Link>{" "}
+              </li>{" "}
+              <li
+                id={location.pathname == "/download" ? "active" : ""}
+                style={{ justifyContent: isOpen ? "" : "center" }}
+              >
+                <Link to="/download">
+                  <RiFolderDownloadFill />
+                  <span style={{ display: isOpen ? "" : "none" }}>
+                    {" "}
+                    Download{" "}
+                  </span>{" "}
+                </Link>{" "}
+              </li>{" "}
+            </ul>{" "}
+          </div>{" "}
+        </div>{" "}
+        <div className="bottom__container">
+          <a href="https://github.com/jhnyc/lihkg-dashboard" target="_blank">
+            <IoLogoGithub />
           </a>{" "}
-        </div>{" "}
-        <div className="links">
-          <ul>
-            <li
-              id={location.pathname == "/" ? "active" : ""}
-              style={{ justifyContent: isOpen ? "" : "center" }}
-            >
-              {/* <IoChatbubbleEllipsesSharp /> */}{" "}
-              <Link to="/">
-                <IoChatbubbleEllipsesSharp />
-                <span style={{ display: isOpen ? "" : "none" }}>
-                  Post Insights{" "}
-                </span>{" "}
-              </Link>{" "}
-            </li>{" "}
-            <li
-              id={location.pathname == "/usermetrics" ? "active" : ""}
-              style={{ justifyContent: isOpen ? "" : "center" }}
-            >
-              <Link to="/usermetrics">
-                <FaUsers />
-                <span style={{ display: isOpen ? "" : "none" }}>
-                  User Insights{" "}
-                </span>{" "}
-              </Link>{" "}
-            </li>{" "}
-            <li
-              id={location.pathname == "/download" ? "active" : ""}
-              style={{ justifyContent: isOpen ? "" : "center" }}
-            >
-              <Link to="/download">
-                <RiFolderDownloadFill />
-                <span style={{ display: isOpen ? "" : "none" }}>
-                  {" "}
-                  Download{" "}
-                </span>{" "}
-              </Link>{" "}
-            </li>{" "}
-          </ul>{" "}
-        </div>{" "}
-      </div>{" "}
+        </div>
+      </div>
     </div>
   );
 }
